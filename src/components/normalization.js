d452366b35f03ff701843caf8a135be4698b6f46
GGolfz <wisarutgolf11@gmail.com>
2021-09-15 23:15:57 +0700

    restructure :wrench:

---
+++ src/components/normalization.js
@@ -0,0 +1,10 @@
+import { Fragment } from "preact";
+
+const Normalization = () => {
+    return (
+        <Fragment>
+            <div>Normalization : Coming Soon ~</div>
+        </Fragment>
+    )
+}
+export default Normalization;
