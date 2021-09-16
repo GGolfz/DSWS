a0f139edf526eb8a6ad1f27e7bf8ac743de46530
GGolfz <wisarutgolf11@gmail.com>
2021-09-16 23:56:58 +0700

    add: mock entropy :sparkles:

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,16 @@
 import { Fragment } from "preact";
 import { useEffect, useState } from "preact/hooks";
+import Entropy from "./entropy";
 import Normalization from "./normalization";
 import Similarity from "./similarity";
 const App = () => {
-  
   return (
-    <div id="app">
+    <div id="app" style={{width:'100vw',height:'100vh',padding:'2rem'}}>
       <Fragment>
-      <Similarity /><Normalization /></Fragment>
+        <Similarity />
+        <Normalization />
+        <Entropy />
+      </Fragment>
     </div>
   );
 };
---
+++ src/components/entropy.js
@@ -0,0 +1,15 @@
+import { Fragment } from "preact";
+
+const Entropy = () => {
+    return (
+        <Fragment>
+            <h1>Entropy Calculator</h1>
+            <div>
+                Coming Soon...
+            </div>
+        </Fragment>
+    )
+
+}
+
+export default Entropy;
