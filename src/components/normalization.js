646dc0de953a05b24aaae0e64d2518627c45c77c
GGolfz <wisarutgolf11@gmail.com>
2021-09-17 17:59:51 +0700

    add: delete row condition :sparkles:

--- src/components/normalization.js
+++ src/components/normalization.js
@@ -13,39 +13,46 @@ const Normalization = () => {
     setData(tempArray);
   };
   const GetZPercent = (z) => {
-  
     if (z < -6.5) {
       return 0.0;
     }
-  
+
     if (z > 6.5) {
       return 1.0;
     }
-  
+
     var factK = 1;
     var sum = 0;
     var term = 1;
     var k = 0;
     var loopStop = Math.exp(-23);
-  
-    while(Math.abs(term) > loopStop) {
-      term = .3989422804 * Math.pow(-1,k) * Math.pow(z,k) / (2 * k + 1) / Math.pow(2,k) * Math.pow(z,k+1) / factK;
+
+    while (Math.abs(term) > loopStop) {
+      term =
+        (((0.3989422804 * Math.pow(-1, k) * Math.pow(z, k)) /
+          (2 * k + 1) /
+          Math.pow(2, k)) *
+          Math.pow(z, k + 1)) /
+        factK;
       sum += term;
       k++;
       factK *= k;
     }
-  
+
     sum += 0.5;
-  
+
     return sum;
-  }
+  };
   const handleCalculate = () => {
     let temp = [...data];
     let minValue = Math.min(...data.map((e) => e.val));
     let maxValue = Math.max(...data.map((e) => e.val));
-    let sum = data.reduce((prev,cur) => prev + cur.val,0);
+    let sum = data.reduce((prev, cur) => prev + cur.val, 0);
     let mean = sum / data.length;
-    let sd = Math.sqrt(data.reduce((prev,cur) => prev + Math.pow((cur.val-mean),2),0) / data.length);
+    let sd = Math.sqrt(
+      data.reduce((prev, cur) => prev + Math.pow(cur.val - mean, 2), 0) /
+        data.length
+    );
     for (let i in temp) {
       let tempObj = { ...temp[i] };
       if (minValue == maxValue) {
@@ -53,19 +60,21 @@ const Normalization = () => {
       } else {
         tempObj.minMax = (tempObj.val - minValue) / (maxValue - minValue);
       }
-      tempObj.zScore = GetZPercent((tempObj.val - mean) / sd)
+      tempObj.zScore = GetZPercent((tempObj.val - mean) / sd);
       temp[i] = tempObj;
     }
     setData(temp);
   };
   const addRow = () => {
-    setData((d) => [...d,{val:0,minMax:0,zScore:0}])
-  }
+    setData((d) => [...d, { val: 0, minMax: 0, zScore: 0 }]);
+  };
   const removeRow = () => {
-    let temp = [...data];
-    temp.pop();
-    setData(temp);
-  }
+    if (data.length > 1) {
+      let temp = [...data];
+      temp.pop();
+      setData(temp);
+    }
+  };
   return (
     <Fragment>
       <h1>Normalization Calculator</h1>
