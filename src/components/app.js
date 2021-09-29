978ba5574e0d514c04cb7de81c7c108aff1340c5
GGolfz <wisarutgolf11@gmail.com>
2021-09-29 20:35:32 +0700

    add: firebase auth :sparkles:

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,78 @@
 import { Fragment } from "preact";
 import { useEffect, useState } from "preact/hooks";
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import Entropy from "./entropy";
 import Naive from "./naive";
 import Normalization from "./normalization";
 import Outlier from "./outlier";
 import Similarity from "./similarity";
 const App = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [app, setApp] = useState(null);
+  const [auth, setAuth] = useState(false);
+  useEffect(() => {
+
+    const FirebaseCredentials = {
+      apiKey: process.env.PREACT_APP_FIREBASE_PUBLIC_API_KEY,
+      authDomain: process.env.PREACT_APP_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.PREACT_APP_FIREBASE_PROJECT_ID,
+    };
+    let app = initializeApp(FirebaseCredentials);
+    setApp(app);
+  }, []);
+  useEffect(() => {
+    if(app!= null) {
+      const auth = getAuth(app);
+      auth.onAuthStateChanged((user) => {
+        if(user) {
+          setAuth(true);
+        }
+      })
+    }
+
+  },[app])
+  const handleSignIn = () => {
+    const auth = getAuth(app);
+    signInWithEmailAndPassword(auth, email, password).then((resp) => {
+      if (resp.user.uid) {
+        setAuth(true);
+      }
+    });
+  };
   return (
     <div id="app" style={{ width: "100vw", height: "100vh", padding: "2rem" }}>
       <Fragment>
-        <Similarity />
-        <Normalization />
-        <Entropy />
-        <Naive />
-        <Outlier />
+        {!auth ? (
+          <div>
+            <input
+              type="text"
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleSignIn}>login</button>
+          </div>
+        ) : null}
+        {auth ? (
+          <Fragment>
+            <Similarity />
+            <Normalization />
+            <Entropy />
+            <Naive />
+            <Outlier />
+          </Fragment>
+        ) : (<div>Please Login to your account</div>)}
       </Fragment>
     </div>
   );
