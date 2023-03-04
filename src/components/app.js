098b7df0d66fbbe79bc01a9c88540d212cd1a809
GGolfz <wisarutgolf11@gmail.com>
2023-03-04 22:13:22 +0700

    remove: authen :fire:

--- src/components/app.js
+++ src/components/app.js
@@ -64,111 +64,86 @@ const App = () => {
   };
   return (
     <div id="app" style={{ width: "100vw", height: "100vh", padding: "2rem" }}>
-      <Fragment>
-        {!auth ? (
-          <div>
-            <input
-              type="text"
-              placeholder="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              type="password"
-              placeholder="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <button onClick={handleSignIn}>login</button>
-          </div>
-        ) : null}
-        {auth ? (
-          <Fragment>
-            <div style={{ display: "flex", flexFlow: "row" }}>
-              <div
-                style={{
-                  padding: "0 2rem",
-                  margin: "0 1rem",
-                  fontWeight: page == "similarity" ? "bold" : "normal",
-                  cursor: "pointer",
-                }}
-                onClick={() => setPage("similarity")}
-              >
-                Similarity
-              </div>
-              <div
-                style={{
-                  padding: "0 2rem",
-                  margin: "0 1rem",
-                  fontWeight: page == "normalization" ? "bold" : "normal",
-                  cursor: "pointer",
-                }}
-                onClick={() => setPage("normalization")}
-              >
-                Normalization
-              </div>
-              <div
-                style={{
-                  padding: "0 2rem",
-                  margin: "0 1rem",
-                  fontWeight: page == "entropy" ? "bold" : "normal",
-                  cursor: "pointer",
-                }}
-                onClick={() => setPage("entropy")}
-              >
-                Entropy
-              </div>
-              <div
-                style={{
-                  padding: "0 2rem",
-                  margin: "0 1rem",
-                  fontWeight: page == "naive" ? "bold" : "normal",
-                  cursor: "pointer",
-                }}
-                onClick={() => setPage("naive")}
-              >
-                Naïve Bayes
-              </div>
-              <div
-                style={{
-                  padding: "0 2rem",
-                  margin: "0 1rem",
-                  fontWeight: page == "outlier" ? "bold" : "normal",
-                  cursor: "pointer",
-                }}
-                onClick={() => setPage("outlier")}
-              >
-                Outlier
-              </div>
-              <div
-                style={{
-                  padding: "0 2rem",
-                  margin: "0 1rem",
-                  fontWeight: page == "prism" ? "bold" : "normal",
-                  cursor: "pointer",
-                }}
-                onClick={() => setPage("prism")}
-              >
-                PRISM
-              </div>
-              <div
-                style={{
-                  padding: "0 2rem",
-                  margin: "0 1rem",
-                  fontWeight: page == "association" ? "bold" : "normal",
-                  cursor: "pointer",
-                }}
-                onClick={() => setPage("association")}
-              >
-                Association
-              </div>
-            </div>
-            {renderItem()}
-          </Fragment>
-        ) : (
-          <div>Please Login to your account</div>
-        )}
-      </Fragment>
+      <div style={{ display: "flex", flexFlow: "row" }}>
+        <div
+          style={{
+            padding: "0 2rem",
+            margin: "0 1rem",
+            fontWeight: page == "similarity" ? "bold" : "normal",
+            cursor: "pointer",
+          }}
+          onClick={() => setPage("similarity")}
+        >
+          Similarity
+        </div>
+        <div
+          style={{
+            padding: "0 2rem",
+            margin: "0 1rem",
+            fontWeight: page == "normalization" ? "bold" : "normal",
+            cursor: "pointer",
+          }}
+          onClick={() => setPage("normalization")}
+        >
+          Normalization
+        </div>
+        <div
+          style={{
+            padding: "0 2rem",
+            margin: "0 1rem",
+            fontWeight: page == "entropy" ? "bold" : "normal",
+            cursor: "pointer",
+          }}
+          onClick={() => setPage("entropy")}
+        >
+          Entropy
+        </div>
+        <div
+          style={{
+            padding: "0 2rem",
+            margin: "0 1rem",
+            fontWeight: page == "naive" ? "bold" : "normal",
+            cursor: "pointer",
+          }}
+          onClick={() => setPage("naive")}
+        >
+          Naïve Bayes
+        </div>
+        <div
+          style={{
+            padding: "0 2rem",
+            margin: "0 1rem",
+            fontWeight: page == "outlier" ? "bold" : "normal",
+            cursor: "pointer",
+          }}
+          onClick={() => setPage("outlier")}
+        >
+          Outlier
+        </div>
+        <div
+          style={{
+            padding: "0 2rem",
+            margin: "0 1rem",
+            fontWeight: page == "prism" ? "bold" : "normal",
+            cursor: "pointer",
+          }}
+          onClick={() => setPage("prism")}
+        >
+          PRISM
+        </div>
+        <div
+          style={{
+            padding: "0 2rem",
+            margin: "0 1rem",
+            fontWeight: page == "association" ? "bold" : "normal",
+            cursor: "pointer",
+          }}
+          onClick={() => setPage("association")}
+        >
+          Association
+        </div>
+      </div>
+      {renderItem()}
     </div>
   );
 };
